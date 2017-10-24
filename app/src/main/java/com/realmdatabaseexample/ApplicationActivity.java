3a0c1dd2a02925a01553e3f4c96e13733f59ccf4
Shiva Singh <singhsiva177@gmail.com>
2017-10-24 12:29:38 +0530

    Update ApplicationActivity.java

--- app/src/main/java/com/realmdatabaseexample/ApplicationActivity.java
+++ app/src/main/java/com/realmdatabaseexample/ApplicationActivity.java
@@ -5,9 +5,7 @@ import android.app.Application;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
-/**
- * Created by esec-sruthi on 21/10/17.
- */
+
 
 public class ApplicationActivity extends Application {
 
